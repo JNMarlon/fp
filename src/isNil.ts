1e4ded32775505f43aa173eddfe9e200b9f4c298
jin <wonjin9497@gmail.com>
2023-03-06 00:19:52 +0900

    isNullable to isNil / add testing

--- src/isNil.ts
+++ src/isNil.ts
@@ -11,7 +11,7 @@ type Nullable<T> = T extends null | undefined ? T : never;
  * isNullable(null); // true
  * ```
  */
-function isNullable<T>(a: T): a is Nullable<T> {
+function isNil<T>(a: T): a is Nullable<T> {
   if (a === undefined || a === null) {
     return true;
   }
@@ -19,4 +19,10 @@ function isNullable<T>(a: T): a is Nullable<T> {
   return false;
 }
 
-export default isNullable;
+/*
+ *
+ *
+ * nil is a representation of nothing.
+ * Its underlying representation is JavaScript's null,
+ * and is equal to JavaScript's undefined*/
+export default isNil;
---
+++ test/isNil.spec.ts
@@ -0,0 +1,26 @@
+import isNil from "../src/isNil";
+
+describe("isNullable", () => {
+  it("should check if given value is `null` or `undefined`", () => {
+    const r1 = isNil(null);
+    expect(r1).toEqual(true);
+
+    const r2 = isNil(undefined);
+    expect(r2).toEqual(true);
+
+    const r3 = isNil(3);
+    expect(r3).toEqual(false);
+
+    const r4 = isNil("testing!");
+    expect(r3).toEqual(false);
+
+    const r5 = isNil({});
+    expect(r4).toEqual(false);
+
+    const r6 = isNil(Symbol("symbol"));
+    expect(r6).toEqual(false);
+
+    const r7 = isNil(false);
+    expect(r7).toEqual(false);
+  });
+});
