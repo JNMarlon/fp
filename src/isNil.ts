f0b5dde63f0ba38661d58e02ffa72512056f0c62
jin <wonjin9497@gmail.com>
2023-03-07 23:50:03 +0900

    fix trivial

--- src/isNil.ts
+++ src/isNil.ts
@@ -1,16 +1,5 @@
 type Nullable<T> = T extends null | undefined ? T : never;
 
-/**
- * Checks if the given value is `null` or `undefined`.
- *
- * @example
- * ```ts
- * isNullable(1); // false
- * isNullable('1'); // false
- * isNullable(undefined); // true
- * isNullable(null); // true
- * ```
- */
 function isNil<T>(a: T): a is Nullable<T> {
   if (a === undefined || a === null) {
     return true;
