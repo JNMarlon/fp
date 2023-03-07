433ef5410989b6073fe911e05fa23523757ea36e
jin <wonjin9497@gmail.com>
2023-03-07 23:53:20 +0900

    add iterableInfer

--- src/@types/@iter.ts
+++ src/@types/@iter.ts
@@ -3,3 +3,7 @@ export type Iter<T = unknown> =
   | AsyncIterator<T>
   | Iterable<T>
   | AsyncIterable<T>;
+
+export type IterableInfer<
+  T extends Iterable<unknown> | AsyncIterable<unknown>
+> = T extends Iterable<infer U> | AsyncIterable<infer U> ? U : never;
