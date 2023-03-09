602dab5271624415265162befbd4e7eea7d0ed81
jin <wonjin9497@gmail.com>
2023-03-09 15:20:01 +0900

    add type tail / typo

--- src/iter.ts
+++ src/iter.ts
@@ -1,27 +1,27 @@
 import type { Iter } from "./@type/@iter";
 
 export function isIterable<T = unknown>(
-  _: Iter<T> | unknown
-): _ is Iterable<T> {
-  return typeof (_ as any)?.[Symbol.iterator] === "function";
+  i: Iter<T> | unknown
+): i is Iterable<T> {
+  return typeof (i as any)?.[Symbol.iterator] === "function";
 }
 
 export function isAsyncIterable<T = unknown>(
-  _: Iter<T> | unknown
-): _ is AsyncIterable<T> {
-  return typeof (_ as any)?.[Symbol.asyncIterator] === "function";
+  i: Iter<T> | unknown
+): i is AsyncIterable<T> {
+  return typeof (i as any)?.[Symbol.asyncIterator] === "function";
 }
 
 export function isIterator<T = unknown>(
-  _: Iter<T>
-): _ is Iterator<T> | AsyncIterator<T> {
-  return typeof (_ as any)?.next === "function";
+  i: Iter<T>
+): i is Iterator<T> | AsyncIterator<T> {
+  return typeof (i as any)?.next === "function";
 }
 
-export function toIterator<T>(_: Iterable<T> | AsyncIterable<T>) {
-  if (isIterable(_)) return _[Symbol.iterator]();
+export function toIterator<T>(i: Iterable<T> | AsyncIterable<T>) {
+  if (isIterable(i)) return i[Symbol.iterator]();
 
-  if (isAsyncIterable(_)) return _[Symbol.asyncIterator]();
+  if (isAsyncIterable(i)) return i[Symbol.asyncIterator]();
 
   throw new TypeError(
     "toIterator: iterable must be type of Iterable or AsyncIterable"
